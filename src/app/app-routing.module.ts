189b63114e4b901fb05e0d5d16e19d190e40124d
Charan <58394919+charanpy@users.noreply.github.com>
2022-01-11 09:01:26 +0530

    test push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppShellComponent } from './components/app-shell/app-shell.component';
 
-
+// test commit
 const routes: Routes = [
   {
     path: 'login',
