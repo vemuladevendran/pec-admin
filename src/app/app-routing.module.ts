0a35b4b5bf51cb6b61557cdaab388af60826bf32
Charan <58394919+charanpy@users.noreply.github.com>
2022-01-11 10:46:41 +0530

    prototype

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,36 +2,68 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppShellComponent } from './components/app-shell/app-shell.component';
 
-// test commit
+// test commit proto
 const routes: Routes = [
   {
     path: 'login',
-    loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule),
+    loadChildren: () =>
+      import('./auth/login/login.module').then((m) => m.LoginModule),
   },
   {
     path: 'signup',
-    loadChildren: () => import('./auth/signup/signup.module').then(m => m.SignupModule),
+    loadChildren: () =>
+      import('./auth/signup/signup.module').then((m) => m.SignupModule),
   },
   {
     path: 'forgot-password',
-    loadChildren: () => import('./auth/forgot-password/forgot-password.module')
-      .then(m => m.ForgotPasswordModule),
+    loadChildren: () =>
+      import('./auth/forgot-password/forgot-password.module').then(
+        (m) => m.ForgotPasswordModule
+      ),
   },
   {
     path: '',
     component: AppShellComponent,
     children: [
-      { path: 'teachers', loadChildren: () => import('./pages/teachers/teachers.module').then(m => m.TeachersModule) },
-      { path: 'students', loadChildren: () => import('./pages/students/students.module').then(m => m.StudentsModule) },
-      { path: 'profile', loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) },
-      { path: 'department', loadChildren: () => import('./pages/department/department.module').then(m => m.DepartmentModule) },
-      { path: 'attendance-report', loadChildren: () => import('./pages/attendance-report/attendance-report.module').then(m => m.AttendanceReportModule) },
-    ]
+      {
+        path: 'teachers',
+        loadChildren: () =>
+          import('./pages/teachers/teachers.module').then(
+            (m) => m.TeachersModule
+          ),
+      },
+      {
+        path: 'students',
+        loadChildren: () =>
+          import('./pages/students/students.module').then(
+            (m) => m.StudentsModule
+          ),
+      },
+      {
+        path: 'profile',
+        loadChildren: () =>
+          import('./pages/profile/profile.module').then((m) => m.ProfileModule),
+      },
+      {
+        path: 'department',
+        loadChildren: () =>
+          import('./pages/department/department.module').then(
+            (m) => m.DepartmentModule
+          ),
+      },
+      {
+        path: 'attendance-report',
+        loadChildren: () =>
+          import('./pages/attendance-report/attendance-report.module').then(
+            (m) => m.AttendanceReportModule
+          ),
+      },
+    ],
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
