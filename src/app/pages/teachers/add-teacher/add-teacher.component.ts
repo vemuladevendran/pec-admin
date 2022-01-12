9d4e92d5f3b4d4cc97a4be69d96a95d67b84a7b4
devendran <devendranvemula@gmail.com>
2022-01-12 12:32:20 +0530

    add teacher complect

--- src/app/pages/teachers/add-teacher/add-teacher.component.ts
+++ src/app/pages/teachers/add-teacher/add-teacher.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-add-teacher',
@@ -7,10 +8,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./add-teacher.component.scss']
 })
 export class AddTeacherComponent implements OnInit {
-
+  @ViewChild('profileUpload') fileInput: ElementRef<HTMLInputElement> | undefined;
+  defaultImageUrl = "/assets/default-profile.png"
   createTeacher: FormGroup
+  selectedImagePreviewURL: any = "";
+  selectedFile: File | undefined;
+  subjects = ['ANT', 'web technology', 'computer Networks', 'Software Engineering']
   constructor(
     private fb: FormBuilder,
+    private sanitizer: DomSanitizer,
+
   ) {
     this.createTeacher = this.fb.group({
       teacherName: ['', Validators.required],
@@ -21,7 +28,26 @@ export class AddTeacherComponent implements OnInit {
     })
   }
 
+  handleSubmit(): void {
+    console.log(this.createTeacher.value);
+  }
+
+  // image change handle
+  handleFileSelection(): void {
+    const [file] = this.fileInput?.nativeElement?.files as any as File[];
+    if (file) {
+      this.selectedFile = file;
+      this.selectedImagePreviewURL = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(file));
+    }
+  }
+
+  // image select
+  openFileSelectionDialog(): void {
+    this.fileInput?.nativeElement?.click();
+  }
+
   ngOnInit(): void {
   }
 
 }
+
