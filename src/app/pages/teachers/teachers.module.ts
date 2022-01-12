7bf12ec27a73f9f00ce1ca5184db7ca4f5a5a20f
devendran <devendranvemula@gmail.com>
2022-01-12 10:10:08 +0530

    clean

--- src/app/pages/teachers/teachers.module.ts
+++ src/app/pages/teachers/teachers.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { TeachersRoutingModule } from './teachers-routing.module';
 import { TeachersComponent } from './teachers.component';
 import { AddTeacherComponent } from './add-teacher/add-teacher.component';
-import { ViewTeacherComponent } from './view-teacher/view-teacher.component';
 
 
 @NgModule({
   declarations: [
     TeachersComponent,
     AddTeacherComponent,
-    ViewTeacherComponent
   ],
   imports: [
     CommonModule,
