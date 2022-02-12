79dc6c61de6937b85e5921c70adb2a8b406d0055
devendran <devendranvemula@gmail.com>
2022-02-12 14:19:48 +0530

    toast service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppShellModule } from './components/app-shell/app-shell.module';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
 
 // HttpClientModule
 
@@ -18,6 +18,12 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserAnimationsModule,
     AppShellModule,
     HttpClientModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      progressBar: true,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/admin/add-admin/add-admin.component.ts
+++ src/app/pages/admin/add-admin/add-admin.component.ts
@@ -1,39 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AdminService } from 'src/app/services/admin/admin.service';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-add-admin',
   templateUrl: './add-admin.component.html',
-  styleUrls: ['./add-admin.component.scss']
+  styleUrls: ['./add-admin.component.scss'],
 })
 export class AddAdminComponent implements OnInit {
   departments: string[] = ['IT', 'CSE', 'ECE', 'EEE', 'MECH'];
-  createAdmin: FormGroup
+  createAdmin: FormGroup;
   constructor(
     private fb: FormBuilder,
     private adminServe: AdminService,
+    private toast: ToastrService
   ) {
     this.createAdmin = this.fb.group({
       name: ['', [Validators.required]],
       mobileNumber: ['', [Validators.required, Validators.minLength(10)]],
       role: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
       //  department: ['', [Validators.required]],
-    })
+    });
   }
 
   async handleSubmit(): Promise<void> {
     try {
       const data = this.createAdmin.value;
-      await this.adminServe.createAdmin(data);
-      console.log(data);
-      
+      const result = await this.adminServe.createAdmin(data);
+      this.toast.success(result);
+      console.log(result);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
